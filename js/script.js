e4d58df92fbc8d96672f88b9a360d6f94f53e026
AndrewPeralta11 <aperalta@aclanyc.com>
2019-10-24 22:28:36 +0000

    started on javascript/jquery

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
+$("button").click(function(){
+    let word = $(input).val();
+    if (word[0] === "a" || word[0] === "e" || word[0] === "i" || word[0] === "o" || word[0] === "u") {
+        $(".output").text(word + "yay");
+    } else {
+        $(".output").text();
+    }
+
+    console.log(word);
+});
 
 
 
-  
-  
-  
-  
 });
