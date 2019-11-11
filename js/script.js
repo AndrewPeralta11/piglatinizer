2b3911773e87b7ed6b7dd6d2328cbc501fa63e89
AndrewPeralta11 <aperalta@aclanyc.com>
2019-11-11 02:12:52 +0000

    Everything Should Work

--- js/script.js
+++ js/script.js
@@ -5,8 +5,8 @@ $("button").click(function() {
         $(".output").text(word + "-" +"yay");
     } else if (word[0] === "b" || word[0] === "c" || word[0] === "d" || word[0] === "f" || word[0] === "g" || word[0] === "h" || word[0] === "j" || word[0] === "k" || word[0] === "l" || word[0] === "m" || word[0] === "n" || word[0] === "p" || word[0] === "q" || word[0] === "r" || word[0] === "s" || word[0] === "t" || word[0] === "v" || word[0] === "w" || word[0] === "x" || word[0] === "y" || word[0] === "z") {
         let firstLetter = word[0].charAt(0);
-        let rtw = word[0].slice(1, word[0].length);
-        $(".output").text(rtw + " " + firstLetter + "ay");
+        let rtw = word.slice(1, word.length);
+        $(".output").text(rtw + "-" + firstLetter + "ay");
     } else {
     }
 });
